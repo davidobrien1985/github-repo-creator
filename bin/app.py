c9679478afd017449b8b7e727220d3e21614c6d6
David OBrien <me@david-obrien.net>
2018-03-25 22:57:44 +1100

    working lambda function

--- bin/app.py
+++ bin/app.py
@@ -14,13 +14,34 @@ def main(event, context):
         
     """
     logger.info('got event %s' % (event))
-    print(event)
     body = convert_body_to_json(event['body'])
     repoName = body['repoName']
     githubBaseUrl = "https://api.github.com"
     githubOwner = "davidobrien1985"
-    createRepository(githubOwner, githubBaseUrl, os.environ['githubPAT'], repoName)
-    configureGitHubBranch(githubOwner, githubBaseUrl, os.environ['githubPAT'], repoName, "master")
+    branches = "master","develop"
+
+    try:
+        logger.info('Create repository {0}'.format(repoName))
+        createRepository(githubOwner, githubBaseUrl, os.environ['githubPAT'], repoName)
+    except (RuntimeError, TypeError, NameError) as err:
+        return respond(err,"{0}".format(err))
+
+    try:
+        logger.info('Create develop branch on {0}'.format(repoName))
+        createGitHubBranch(githubOwner, repoName, githubBaseUrl, os.environ['githubPAT'], "develop")
+    except (RuntimeError, TypeError, NameError) as err:
+        return respond(err,"{0}".format(err))
+
+    # configure both master and develop branch on new repo
+    for branch in branches:
+        try:
+            logger.info('Configuring branch {0} on repo {1}'.format(branch, repoName))
+            configureGitHubBranch(githubOwner, githubBaseUrl, os.environ['githubPAT'], repoName, branch)
+        except (RuntimeError, TypeError, NameError) as err:
+            return respond(err,"{0}".format(err))
+
+    return respond(None,"Successfully created GitHub repo {0}".format(repoName))
+
 
 def respond(err, res=None):
     return {
@@ -66,7 +87,7 @@ def createRepository(githubOwner, githubBaseUrl, githubPat, githubRepoName):
     jsonResponse = json.loads(response.content)
     print("Created repository {repoName} at {time}".format(repoName=jsonResponse['full_name'], time=date.datetime.now()))
 
-def createGitHubBranch(githubOwner, githubBaseUrl, githubPat, githubBranchName):
+def createGitHubBranch(githubOwner, githubRepoName, githubBaseUrl, githubPat, githubBranchName):
     """create a GitHub branch
     
     Arguments:
@@ -75,6 +96,34 @@ def createGitHubBranch(githubOwner, githubBaseUrl, githubPat, githubBranchName):
         githubBranch {string} -- GitHub branch to configure
         githubBaseUrl {string} -- base Url to the GitHub API
     """
+
+    headers = {
+        'Authorization': 'token %s' % githubPat,
+        "User-Agent": "githubmanage-python"
+    }
+
+    sha = r.get(
+        githubBaseUrl + "/repos/{0}/{1}/git/refs/heads".format(githubOwner, githubRepoName),
+        headers=headers
+    )
+
+    content = json.loads(sha.content)
+
+    sha = content[0]["object"]["sha"]
+
+    payload = {
+        "ref": "refs/heads/{0}".format(githubBranchName),
+        "sha": sha
+    }
+
+    response = r.post(
+        githubBaseUrl + "/repos/{0}/{1}/git/refs".format(githubOwner, githubRepoName),
+        data=json.dumps(payload),
+        json=None,
+        headers=headers)
+
+    jsonResponse = json.loads(response.content)
+    print(jsonResponse)
 
 
 def configureGitHubBranch(githubOwner, githubBaseUrl, githubPat, githubRepoName, githubBranch):
